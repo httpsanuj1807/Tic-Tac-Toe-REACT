2e082b15320f8c10e915d6db2666ba671c7223a9
Anuj Kumar <anuj1316.be22@chitkara.edu.in>
2024-02-14 15:02:50 +0530

    Optimization

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import { useState } from "react";
 import GameOver from "./Components/GameOver";
 import WINNING_COMBINATIONS from "../winningCombination";
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null , null],
   [null, null , null],
   [null, null , null],
 ]
+
+const PLAYERS_NAME = {
+  X : 'Player 1',
+  O : 'Player 2'
+}
 
 function getActivePlayer(prevValuesOrGamesTurns){
   let currentPlayer = 'X';
@@ -19,12 +24,40 @@ function getActivePlayer(prevValuesOrGamesTurns){
   return currentPlayer;
 }
 
+function checkWinner(gameBoard,playersName){
+  let winner;
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if( firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = playersName[firstSquareSymbol];
+    }
+  }
+  return winner;
+}
+
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
+  for (const turn of gameTurns){
+      const {square, player} = turn;
+      const {row, col} = square;
+      gameBoard[row][col] = player;
+  }  
+  return gameBoard;
+}
+
 function App() {
 
-  const [playersName, setPlayersName] = useState({
-    X : 'Player 1',
-    Y : 'Player 2',
-  });
+  const [playersName, setPlayersName] = useState(PLAYERS_NAME);
+
+  
+  const [gameTurns, setGameTurns] = useState([]);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = checkWinner(gameBoard,playersName);
+  const hasDraw = gameTurns.length === 9 && !winner;
+  const activePlayer = getActivePlayer(gameTurns);
 
   function handlePlayerNameChange(symbol, newName){
     setPlayersName((prevState)=>{
@@ -35,30 +68,6 @@ function App() {
     })
   }
     
-  const [gameTurns, setGameTurns] = useState([]);
- 
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
-  for (const turn of gameTurns){
-      const {square, player} = turn;
-      const {row, col} = square;
-      gameBoard[row][col] = player;
-  }  
-
-  let winner;
-
-  for(const combination of WINNING_COMBINATIONS){
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-
-    if( firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
-      winner = playersName[firstSquareSymbol];
-    }
-  }
-
-  const hasDraw = gameTurns.length === 9 && !winner;
-
-  const activePlayer = getActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex){
     setGameTurns((prevValues)=>{
       const currentPlayer = getActivePlayer(prevValues);
@@ -84,8 +93,8 @@ function App() {
     <main> 
       <div id="game-container"> 
         <ol id="players" className="highlight-player">
-        <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"} onNameChange={handlePlayerNameChange}/> 
-        <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} onNameChange={handlePlayerNameChange} /> 
+        <Player initialName={PLAYERS_NAME.X} symbol={"X"} isActive={activePlayer === "X"} onNameChange={handlePlayerNameChange}/> 
+        <Player initialName={PLAYERS_NAME.O} symbol={"O"} isActive={activePlayer === "O"} onNameChange={handlePlayerNameChange} /> 
         </ol>
         {(winner || hasDraw) && <GameOver onRestart={handleRematch} winner={winner}/>}
         <GameBoard onSelectSquare={handleSelectSquare} gameBoard={gameBoard}/>
--- src/Components/GameOver.jsx
+++ src/Components/GameOver.jsx
@@ -4,7 +4,7 @@ function GameOver({winner, onRestart}){
     return (
         <div id="game-over">
             <h2>Game Over!</h2>
-            {winner && <p>{winner} won!</p> }
+            {winner && <p>{winner.toUpperCase()} won!</p> }
             {!winner && <p>It&apos;s a draw!</p> }
             <p>
                 <button onClick={onRestart}>Rematch!</button>
