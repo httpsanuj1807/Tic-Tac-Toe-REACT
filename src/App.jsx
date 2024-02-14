219dff1902e9a09c7ec90427e1381ef98127d7f8
Anuj Kumar <anuj1316.be22@chitkara.edu.in>
2024-02-14 11:57:04 +0530

    Game Over and Resetting Game Board

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import Player from "./Components/Player";
 import GameBoard from "./Components/GameBoard";
 import Logs from "./Components/Logs";
 import { useState } from "react";
+import GameOver from "./Components/GameOver";
 import WINNING_COMBINATIONS from "../winningCombination";
+
+const initialGameBoard = [
+  [null, null , null],
+  [null, null , null],
+  [null, null , null],
+]
 
 function getActivePlayer(prevValuesOrGamesTurns){
   let currentPlayer = 'X';
@@ -13,7 +20,30 @@ function getActivePlayer(prevValuesOrGamesTurns){
 }
 
 function App() {
+
   const [gameTurns, setGameTurns] = useState([]);
+ 
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+  for (const turn of gameTurns){
+      const {square, player} = turn;
+      const {row, col} = square;
+      gameBoard[row][col] = player;
+  }  
+
+  let winner;
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if( firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   const activePlayer = getActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex){
     setGameTurns((prevValues)=>{
@@ -32,6 +62,10 @@ function App() {
     })
   }
 
+  function handleRematch(){
+    setGameTurns([]);
+  }
+
   return (
     <main> 
       <div id="game-container"> 
@@ -39,7 +73,8 @@ function App() {
         <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === "X"}/> 
         <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === "O"} /> 
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        {(winner || hasDraw) && <GameOver onRestart={handleRematch} winner={winner}/>}
+        <GameBoard onSelectSquare={handleSelectSquare} gameBoard={gameBoard}/>
       </div>
       <Logs turns={gameTurns} />
     </main>
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,19 +1,10 @@
 // import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null , null],
-    [null, null , null],
-    [null, null , null],
-]
 
-function GameBoard({onSelectSquare, turns}){
 
-    let gameBoard = initialGameBoard;
-    for (const turn of turns){
-        const {square, player} = turn;
-        const {row, col} = square;
-        gameBoard[row][col] = player;
-    }
+function GameBoard({onSelectSquare, gameBoard}){
+
+   
     // const [gameBoard , setGameBoard] = useState(initialGameBoard);
     
     // function buttonClickHandler(rowIndex, colIndex){
